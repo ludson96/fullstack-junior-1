d7d26da3e5dbded4c6e0be282d3721783507ee64
Ludson Pereira <ludson_ps27@hotmail.com>
2024-04-11 17:35:13 -0300

    feat: :adhesive_bandage: fix unauthorized message

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,9 +5,11 @@ export function middleware(request: NextApiRequest, response: NextApiResponse) {
   const headersList = headers()
   const secret = headersList.get('secret')
 
-  if (secret !== 'naranja-labs') {
+  const FIXED_SECRET = 'naranja-labs';
+
+  if (secret !== FIXED_SECRET) {
     return Response.json(
-      { success: false, message: 'Secret inválido' },
+      { message: 'Secret provided is invalid. Make sure the correct secret is being sent' },
       { status: 401 }
     )
   }
