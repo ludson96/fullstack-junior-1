3c5218009acb4b428a5199590493c2592a618c7e
Ludson Pereira <ludson_ps27@hotmail.com>
2024-04-11 19:57:47 -0300

    feat: :sparkles: add specific job GET dynamic route

---
+++ src/app/jobs/[id]/route.ts
@@ -0,0 +1,19 @@
+import { jobs } from '../jobs';
+
+export async function GET(request: Request, { params }: { params: { id: string}} ) {
+  if (params.id === undefined || !params.id) return Response.json({ message: 'Job ID is required.' }, { status: 400 });
+
+  // Verifica se o id é um número, caso não seja retorna um erro personalizado
+  if (Number.isNaN(Number(params.id))) {
+    return Response.json({ message: 'ID must be a number' }, { status: 404 });
+  }
+
+  const job = jobs.find((job) => job.id === Number(params.id));
+
+  if (!job) {
+    return Response.json({ message: 'Job ID not found.' }, { status: 404 });
+  }
+
+
+  return Response.json(job);
+}
