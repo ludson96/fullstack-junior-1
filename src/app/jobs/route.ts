854075001887a3456d6303350e468221677bdbfd
Ludson Pereira <ludson_ps27@hotmail.com>
2024-04-11 17:58:21 -0300

    feat: :sparkles: add level junior GET filter route

--- src/app/jobs/route.ts
+++ src/app/jobs/route.ts
@@ -1,5 +1,9 @@
+import { NextRequest } from 'next/server';
 import { jobs } from './jobs';
 
-export async function GET() {
-  return Response.json(jobs);
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const query = searchParams.get('level');
+  const filteredJobs = query ? jobs.filter((job) => job.level === query) : jobs;
+  return Response.json(filteredJobs);
 }
