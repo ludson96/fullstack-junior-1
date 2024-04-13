3b36791abe6ae470955054436bcb278f75b5c51b
Ludson Pereira <ludson_ps27@hotmail.com>
2024-04-13 19:05:19 -0300

    refactor: :recycle: refactor classes to services

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,57 +1,41 @@
 /* eslint-disable max-lines-per-function */
 import Image from 'next/image';
 import Link from 'next/link';
-
-
+import { merriweather, poppins } from '../utils/fonts';
 
 export default function Home() {
   return (
     <main>
-      <div className= 'absolute w-[784px] h-[303px] top-[216px] left-[160px]' >
-      <h1 className={`${merriweather.className}
-       not-italic font-bold text-[80px] leading-[101px] text-white`}>
-      Build your best community starting from here.
-      </h1>
+      {/* Título principal */}
+      <div className= 'custom-div-build' >
+        <h1 className={`custom-h1-build ${merriweather.className}`}>
+        Build your best community starting from here.
+        </h1>
       </div>
-
-      <div className='absolute w-[648px] h-[120px] top-[543px] left-[160px] opacity-50'>
-        <p
-        className={
-          `${poppins.className} text-[24px] font-normal leading-[40px] left-white`
-          }>
-        Meet and communicate with the best people to run projects, events or other
-        activities in a more effective and fun way
+      {/* Paragrafo abaixo do título  */}
+      <div className='custom-div-meet'>
+        <p className={`${poppins.className} custom-p-meet`}>
+          Meet and communicate with the best people to run projects, events or other
+          activities in a more effective and fun way
         </p>
       </div>
 
-      <div className='w-[420px] custom-position-group1 custom-rounded'>
-        <div className='w-[240px] custom-position-group1 custom-rounded'>
-          <button
-          className={`absolute 
-           w-[240px] h-[60px] top-[695px] left-[160px] custom-rounded bg-[#DB2A6B] center-text hover:bg-red-700`}
-          >
-          <p className={
-            `${poppins.className}
-            h27-t18-l27 font-normal left-white w-[103px] `
-            }>
-            Get Started
-          </p>
+      <div className='custom-div-cont-group1'>
+        {/* Botão 'Get Started' */}
+        <div className='custom-div-btn'>
+          <button className='custom-btn'>
+            <p className={`${poppins.className} custom-p-started`}>
+              Get Started
+            </p>
           </button>
-
         </div>
+          {/* Texto 'Learn More' e seta */}
+        <Link className='custom-link' href='/'>
 
-        <Link
-        className='absolute w-[148px] h-[40px] top-[705px] left-[432px] flex items-center justify-between'
-        href='/'
-        >
-
-          <p
-          className={
-            `h-[27px]
-             ${poppins.className} text-[18px] font-semibold leading-[27px] left-white underline`
-          }>
-          Learn More
+          <p className={`${poppins.className} custom-p-learn`}>
+            Learn More
           </p>
+
           <Image
             src='/images/arrow.svg'
             alt='image arrow'
@@ -61,11 +45,13 @@ export default function Home() {
           />
 
         </Link>
-
       </div>
 
-      <div className='flex gap-[48px] absolute w-[616px]  h-[64px] top-[899px] left-[160px] '>
-        <div className='flex items-center gap-[16px] w-[190px] h-[64px]'>
+      {/* Services */}
+      <div className='custom-cont-div-services'>
+
+        <div className='custom-div-community'>
+
           <Image
             src='/images/iconBuild.svg'
             alt='image icon build community'
@@ -73,13 +59,11 @@ export default function Home() {
             height={64}
             className='rounded-[16px]'
           />
-          <p
-            className={
-              `w-[110px] h-[54px]
-               ${poppins.className} text-[18px] font-semibold leading-[27px] left-white`
-            }>
+
+          <p className={`${poppins.className} custom-p-community`}>
             Build Community
           </p>
+
         </div>
 
         <div className='flex items-center gap-[16px] w-[141px] h-[64px]  '>
