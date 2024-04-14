18fd38dcf5a56cdd2957096f12a9513003260a17
Ludson Pereira <ludson_ps27@hotmail.com>
2024-04-14 18:51:03 -0300

    refactor: :recycle: Make a map with people's images

--- src/components/PersonImage.tsx
+++ src/components/PersonImage.tsx
@@ -1,48 +1,20 @@
 import Image from 'next/image';
+import personData from '../data/personData';
 
 // Este componente renderiza imagens de pessoas em posição absoluta.
 export default function PersonImage() {
   return (
     <>
-      <Image
-        src='/images/personBlue.svg'
-        alt='image person background blue'
-        width={240}
-        height={240}
-        className='custom-img-pblue'
-      />
-
-      <Image
-        src='/images/personPurple.svg'
-        alt='image person background blue'
-        width={240}
-        height={509}
-        className='custom-img-ppurple'
-      />
-
-      <Image
-        src='/images/personPink.svg'
-        alt='image person background blue'
-        width={240}
-        height={240}
-        className='custom-img-ppink'
-      />
-
-      <Image
-        src='/images/personOrange.svg'
-        alt='image person background blue'
-        width={240}
-        height={509}
-        className='custom-img-porange'
-      />
-
-      <Image
-        src='/images/personYellow.svg'
-        alt='image person background blue'
-        width={240}
-        height={509}
-        className='custom-img-yellow'
-      />
+      {personData.map((data) => (
+            <Image
+              key={data.id}
+              src={data.src}
+              alt={data.alt}
+              width={data.width}
+              height={data.height}
+              className={data.className}
+            />
+      ))} {/* Aqui está o parêntese de fechamento que estava faltando */}
     </>
   );
 }
---
+++ src/data/personData.ts
@@ -0,0 +1,47 @@
+// Define os dados de navegação para o menu no cabeçalho
+const personData = [
+  {
+    id: '1',
+    src: '/images/personBlue.svg',
+    alt: 'image person background blue',
+    width: 240,
+    height: 240,
+    className: 'custom-img-pblue',
+  },
+  {
+    id: '2',
+    src: '/images/personPurple.svg',
+    alt: 'image person background purple',
+    width: 240,
+    height: 509,
+    className: 'custom-img-ppurple',
+  },
+  {
+    id: '3',
+    src: '/images/personPink.svg',
+    alt: 'image person background pink',
+    width: 240,
+    height: 240,
+    className: 'custom-img-ppink',
+  },
+
+  {
+    id: '4',
+    src: '/images/personOrange.svg',
+    alt: 'image person background orange',
+    width: 240,
+    height: 509,
+    className: 'custom-img-porange',
+  },
+
+  {
+    id: '5',
+    src: '/images/personYellow.svg',
+    alt: 'image person background yellow',
+    width: 240,
+    height: 509,
+    className: 'custom-img-yellow',
+  },
+];
+
+export default personData;
